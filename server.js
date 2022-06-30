453ac98833bb51380de6206118b061340ed7cb8e
João Paulo <joaopaulo_seixas@outlook.com>
2022-06-29 23:45:25 -0300

    update Project for deploy on Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-var cors = require("cors");
+const cors = require("cors");
 app.use(cors());
 const knex = require("knex");
 const bcrypt = require("bcrypt");
