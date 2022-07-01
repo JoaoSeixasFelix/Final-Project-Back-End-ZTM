807d49dfd1514347054d4c18840a34d278a5dac8
João Paulo <joaopaulo_seixas@outlook.com>
2022-07-01 00:38:24 -0300

    update Project and update headers in cors for deploy on Heroku

--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const signin = require("./controllers/signin");
 const image = require("./controllers/image");
 const profile = require("./controllers/profile");
 const saltRounds = 10;
-const options = require("./cors/options");
+// const options = require("./cors/options");
 app.use(favicon(__dirname + "/favicon.ico"));
 app.use(express.json());
 app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
-app.use(cors(options.handleHeaders));
+const allowedOrigins = "*";
+const options = {
+  origin: allowedOrigins,
+};
+app.use(cors(options));
 
 const db = knex({
   client: "pg",
