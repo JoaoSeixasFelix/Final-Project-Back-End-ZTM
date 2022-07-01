aced0962e9b49e2bad2718c95bdce24e068536d6
João Paulo <joaopaulo_seixas@outlook.com>
2022-07-01 01:11:39 -0300

    update Project and update headers in cors for deploy on Heroku

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,31 +5,27 @@ const handleSignUp = (req, res, db, bcrypt, saltRounds) => {
   }
   const salt = bcrypt.genSaltSync(saltRounds);
   const hash = bcrypt.hashSync(password, salt);
-  try {
-    db.transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          email: email,
-        })
-        .into("login")
-        .returning("email")
-        .then((loginEmail) => {
-          return trx("users")
-            .returning("*")
-            .insert({
-              email: loginEmail[0].email,
-              name: name,
-              joined: new Date(),
-            })
-            .then((user) => res.json(user[0]));
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    });
-  } catch (err) {
-    res.status(400).json("Password or Email wrong.");
-  }
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => res.json(user[0]));
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  });
 };
 
 module.exports = {
