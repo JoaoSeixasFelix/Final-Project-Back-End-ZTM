5b08d05ce512b8efe904f62c8bac25791e4b0de3
João Paulo <joaopaulo_seixas@outlook.com>
2022-07-07 13:27:24 -0300

    config and update connection with DataBase

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,28 +5,29 @@ const handleSignUp = (req, res, dataBase, bcrypt, saltRounds) => {
   }
   const salt = bcrypt.genSaltSync(saltRounds);
   const hash = bcrypt.hashSync(password, salt);
-  dataBase.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0].email,
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => res.json(user[0]));
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  });
-  // .catch((err) => res.status(400).json("unable to register"));
+  dataBase
+    .transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .returning("*")
+            .insert({
+              email: loginEmail[0].email,
+              name: name,
+              joined: new Date(),
+            })
+            .then((user) => res.json(user[0]));
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("unable to register"));
 };
 
 module.exports = {
